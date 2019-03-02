11251028ffdbc34ca950d05305a1219ff8bc1197
Matt McGrath <mcm223@g.harvard.edu>
2019-03-01 22:50:02 -0800

    Set up Twitter and Bing dependencies

---
+++ witch-hunter.py
@@ -0,0 +1,8 @@
+# Twitter API
+import twython
+
+# Bing Image Search
+from azure.cognitiveservices.search.imagesearch import ImageSearchAPI
+from msrest.authentication import CognitiveServicesCredentials
+
+print("Hello world")

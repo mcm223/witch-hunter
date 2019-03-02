dab38a2dc291b14c3d62fd696d97164bda6e9dbd
Matt McGrath <mcm223@g.harvard.edu>
2019-03-02 00:16:43 -0800

    Add Bing image search

--- witch-hunter.py
+++ witch-hunter.py
@@ -8,6 +8,9 @@ from twython import Twython
 from azure.cognitiveservices.search.imagesearch import ImageSearchAPI
 from msrest.authentication import CognitiveServicesCredentials
 
+# Other stuff
+import json
+
 # Set up Twython
 t = Twython(
         settings.twitter_app_key,
@@ -15,4 +18,12 @@ t = Twython(
         settings.twitter_oauth_token,
         settings.twitter_oauth_token_secret)
 
+# Set up Bing API
+b = ImageSearchAPI(CognitiveServicesCredentials(settings.bing_key_one))
 
+# Test out image search
+res = b.images.search(query='witch')
+first = res.value[0]
+print("Total number of images: {}".format(len(res.value)))
+print("First image thumbnail url: {}".format(first.thumbnail_url))
+print("First image content url: {}".format(first.content_url))

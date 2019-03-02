e55c820964450318777c060bd85689e2518ffa09
Matt McGrath <mcm223@g.harvard.edu>
2019-03-01 23:52:59 -0800

    Add Oauth tokens to post Tweets

--- witch-hunter.py
+++ witch-hunter.py
@@ -2,9 +2,17 @@
 from settings import settings
 
 # Twitter API
-import twython
+from twython import Twython
 
 # Bing Image Search
 from azure.cognitiveservices.search.imagesearch import ImageSearchAPI
 from msrest.authentication import CognitiveServicesCredentials
+
+# Set up Twython
+t = Twython(
+        settings.twitter_app_key,
+        settings.twitter_app_secret,
+        settings.twitter_oauth_token,
+        settings.twitter_oauth_token_secret)
+
 

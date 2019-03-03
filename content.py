5308541bb9fa90eaf4a5bcf561dd9a0a04b82690
Matt McGrath <mcm223@g.harvard.edu>
2019-03-03 11:32:12 -0800

    Add content file

---
+++ content.py
@@ -0,0 +1,19 @@
+class content:
+    responses = [
+            "Hey @realDonaldTrump, it seems like you're having trouble finding witches so I brought you one!",
+            "Did someone mention a witch?! Lucky for @realDonaldTrump I have one handy:",
+            "Oooh, witches! Spooky! @realDonaldTrump you might like this witch I found for you:",
+            "A witch hunt, how fun @realDonaldTrump! Look I found one already:",
+            "Seems like @realDonaldTrump can't find witches very well, so here's one I found:",
+            "Beep, boop. I'm a bot who helps @realDonaldTrump find the witches he can't seem to locate:"
+            ]
+
+    statuses = [
+            "Looks like old man Trump is complaining about witches again! 🤷‍♂️",
+            "Ooh spooky! Sounds like someone is a little worried about witches... 🤔",
+            "Methinks the president doth protest too much...but I'm just a bot so what do I know? 🤖",
+            "Heyyy wait it's not Halloween, so what's with all the witch talk? What a letdown 😢",
+            "Hey @MichaelCohen212, could you help Donald find his witches? He seems very concerned about them 🙏"
+            ]
+
+
